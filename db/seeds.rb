27561aa2de78b584fe829eb1e5d0f8313a229445
Nikki Veen <nikkiveen@gmail.com>
2016-04-10 15:28:36 -0500

    cleaned up misspellings in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,13 @@
     :name => Faker::App.name,
     :address => "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr}, #{Faker::Address.zip}",
     :email => Faker::Internet.email,
-    :phone_number => Faker::PhoneNumber.phone_number,
+    :phone => Faker::PhoneNumber.phone_number,
     :bio => Faker::Lorem.paragraph(4),
-    :resturant_admin_id => rand(1..3)
+    :restaurant_admin_id => rand(1..3)
   )
 end
 
-5.time do
+5.times do
   Seat.create(
     status: "Available",
     restaurant_id: 1
