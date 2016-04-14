48082eef0420cb1e17371313e280b7434a49fa57
Nikki Veen <nikkiveen@gmail.com>
2016-04-13 19:10:36 -0500

    added date column to reservation table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410191642) do
+ActiveRecord::Schema.define(version: 20160414000637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20160410191642) do
     t.integer  "seat_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
   end
 
   create_table "restaurant_admins", force: :cascade do |t|
