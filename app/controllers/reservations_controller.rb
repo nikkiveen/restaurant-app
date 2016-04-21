ba11cedc7e6a1ac731786b9468a94838431713c0
Nikki Veen <nikkiveen@gmail.com>
2016-04-20 21:37:47 -0500

    updated the reservation create method to be able to do multiple seat resos

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,22 @@
 class ReservationsController < ApplicationController
   def create
-    available_seat = Seat.where("restaurant_id = ? AND date = ? AND timeslot_id = ? AND status = ?", params[:restaurant_id], params[:reservation][:date], params[:timeslot][:timeslot_id], "Available").first
+    head_count = params[:head_count].to_i
+    available_seats = Seat.where("restaurant_id = ? AND date = ? AND timeslot_id = ? AND status = ?", params[:restaurant_id], params[:reservation][:date], params[:timeslot][:timeslot_id], "Available")
 
-    if available_seat
+    if available_seats.length >= head_count
       Reservation.create(
-        seat_id: available_seat.id,
         diner_id: current_diner.id, 
         date: params[:reservation][:date],
-        timeslot_id: params[:timeslot][:timeslot_id]
+        timeslot_id: params[:timeslot][:timeslot_id],
+        head_count: params[:head_count]
       )
 
-      available_seat.update(
-        status: "Reserved"
-      )
+      head_count.times do 
+        available_seat = Seat.where("restaurant_id = ? AND date = ? AND timeslot_id = ? AND status = ?", params[:restaurant_id], params[:reservation][:date], params[:timeslot][:timeslot_id], "Available").first
+        available_seat.update(
+          status: "Reserved"
+        )
+      end
 
       flash[:success] = "Seat successfully reserved!"
     else
