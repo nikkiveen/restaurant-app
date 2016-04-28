a356418fa44608e16ac24369e7c4fe5a804d0209
Nikki Veen <nikkiveen@gmail.com>
2016-04-27 20:14:16 -0500

    added form to create a new restaurant and updated new/create methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,9 +5,26 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
+    render 'new.html.erb'
   end
 
   def create
+    @restaurant = Restaurant.new(
+      name: params[:input_name],
+      address: params[:input_address],
+      phone: params[:input_phone],
+      email: params[:input_email],
+      bio: params[:input_bio],
+      max_reservation_size: params[:input_max_reservation_size],
+      restaurant_admin_id: params[:restaurant_admin_id]
+    )
+
+    if @restaurant.save
+      redirect_to "/restaurants/#{@restaurant.id}"
+    else
+      render 'new.html.erb'
+    end
   end
 
   def show
@@ -16,5 +33,4 @@ class RestaurantsController < ApplicationController
     @head_counts = [1, 2, 3, 4, 5]
     render 'show.html.erb'
   end
-
 end
