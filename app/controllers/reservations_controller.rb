a4946bfa24620f8fb6af52a2bd382abe4f6f9a68
Nikki Veen <nikkiveen@gmail.com>
2016-05-09 20:20:16 -0500

    updated create action for reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,24 +1,13 @@
 class ReservationsController < ApplicationController
   def create
-    head_count = params[:head_count].to_i
-    available_seats = Seat.where("restaurant_id = ? AND date = ? AND timeslot_id = ? AND status = ?", params[:restaurant_id], params[:reservation][:date], params[:timeslot][:timeslot_id], "Available")
-
-    if available_seats.length >= head_count
-      reservation = Reservation.create(
-        diner_id: current_diner.id, 
-        date: params[:reservation][:date],
-        timeslot_id: params[:timeslot][:timeslot_id],
-        head_count: params[:head_count]
-      )
-
-      head_count.times do 
-        available_seat = Seat.where("restaurant_id = ? AND date = ? AND timeslot_id = ? AND status = ?", params[:restaurant_id], params[:reservation][:date], params[:timeslot][:timeslot_id], "Available").first
-        available_seat.update(
-          status: "Reserved",
-          reservation_id: reservation.id
-        )
-      end
-
+    reservation = Reservation.new(
+      diner_id: current_diner.id,
+      date: params[:reservation][:date],
+      timeslot_id: params[:timeslot][:timeslot_id],
+      restaurant_id: params[:restaurant_id],
+      head_count: params[:head_count]
+    )
+    if restaurant.save
       flash[:success] = "Seat successfully reserved!"
     else
       flash[:error] = "No Seats Available!"  
