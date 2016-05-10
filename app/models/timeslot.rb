1684695a72ef070c7c883b6b40a3f7b5583f2181
Nikki Veen <nikkiveen@gmail.com>
2016-05-09 20:11:18 -0500

    updated reservation table

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,4 +1,3 @@
 class Timeslot < ActiveRecord::Base
-  belongs_to :restaurant
   has_many :seats 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160510004733) do
+ActiveRecord::Schema.define(version: 20160510010807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,12 @@ ActiveRecord::Schema.define(version: 20160510004733) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "diner_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.date     "date"
     t.integer  "head_count"
     t.integer  "timeslot_id"
+    t.integer  "restaurant_id"
   end
 
   create_table "restaurant_admins", force: :cascade do |t|
