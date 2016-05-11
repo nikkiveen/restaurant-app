c1f5e6bb9adb975ad3dbfe0d34f8e078a89b389b
Nikki Veen <nikkiveen@gmail.com>
2016-05-10 20:39:37 -0500

    updated restaurant create form with new fields

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,17 +6,26 @@ class RestaurantsController < ApplicationController
 
   def new
     @restaurant = Restaurant.new
+    @timeslots = Timeslot.all
     render 'new.html.erb'
   end
 
   def create
+    @timeslots = Timeslot.all
     @restaurant = Restaurant.new(
       name: params[:input_name],
       address: params[:input_address],
+      city: params[:input_city],
+      state: params[:input_state],
+      zip: params[:input_zip],
       phone: params[:input_phone],
       email: params[:input_email],
       bio: params[:input_bio],
+      total_seats: params[:input_total_seats],
       max_reservation_size: params[:input_max_reservation_size],
+      seats_per_timeslot: params[:input_seats_per_timeslot],
+      open_timeslot: params[:input_open_timeslot][:timeslot_id],
+      close_timeslot: params[:input_close_timeslot][:timeslot_id],
       restaurant_admin_id: params[:restaurant_admin_id]
     )
 
@@ -41,6 +50,7 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @timeslots = Timeslot.all
     @restaurant = Restaurant.find_by(id: params[:id])
     render 'edit.html.erb'
   end
